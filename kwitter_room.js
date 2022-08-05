b5b26c11cbe94ed14afbc3614d424e0961c13598
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2022-08-05 19:07:42 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,13 @@
-
+var firebaseConfig = {
+    apiKey: "AIzaSyCc6u2fmFtKLCc-9nDUzDG9_tcEuTFcGLo",
+    authDomain: "project94-a2a07.firebaseapp.com",
+    projectId: "project94-a2a07",
+    storageBucket: "project94-a2a07.appspot.com",
+    messagingSenderId: "83854043617",
+    appId: "1:83854043617:web:bc6a76661d3ef717428aa0"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
 
 
